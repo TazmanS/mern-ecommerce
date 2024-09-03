6079fa3d176df41e794afe0e74fc958028fed70e
TazmanS <tazmans123@gmail.com>
2024-09-03 15:01:49 +0300

    signup controller

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,5 +1,19 @@
+import User from "../models/user.module.js";
+
 export const signUp = async (req, res) => {
-    res.send('signup call');
+    const {email, password, name} = req.body;
+    try {
+        const userExists = await User.findOne({email});
+        if (userExists) {
+            return res.status(400).json({error: 'User already exists'});
+        }
+
+        const user = await User.create({name, email, password});
+        return res.status(201).json({user, message: 'User created successfully'});
+    } catch (error) {
+        res.status(400).json({error: error.message});
+    }
+
 }
 
 export const login = async (req, res) => {
