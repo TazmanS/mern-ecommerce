77723ec5d54f8e19c6774cb8565eeb22a7a37115
TazmanS <tazmans123@gmail.com>
2024-09-03 18:02:18 +0300

    auth signup back

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,25 +1,84 @@
 import User from "../models/user.module.js";
+import jwt from "jsonwebtoken";
+import { redis } from "../lib/redis.js";
+
+const generateTokens = (userId) => {
+  const accessToken = jwt.sign(
+    { id: userId },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: "15m",
+    },
+  );
+
+  const refreshToken = jwt.sign(
+    { id: userId },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: "7d",
+    },
+  );
+
+  return { accessToken, refreshToken };
+};
+
+const storeRefreshToken = async (userId, refreshToken) => {
+  await redis.set(
+    `refresh_token:${userId}`,
+    refreshToken,
+    "EX",
+    7 * 24 * 60 * 60, // 7days
+  );
+};
+
+const setCookies = (res, accessToken, refreshToken) => {
+  res.cookie("accessToken", accessToken, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "strict",
+    maxAge: 15 * 60 * 1000, // 15min
+  });
+  res.cookie("refreshToken", refreshToken, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "strict",
+    maxAge: 7 * 24 * 60 * 60 * 1000, // 7days
+  });
+};
 
 export const signUp = async (req, res) => {
-    const {email, password, name} = req.body;
-    try {
-        const userExists = await User.findOne({email});
-        if (userExists) {
-            return res.status(400).json({error: 'User already exists'});
-        }
-
-        const user = await User.create({name, email, password});
-        return res.status(201).json({user, message: 'User created successfully'});
-    } catch (error) {
-        res.status(400).json({error: error.message});
+  const { email, password, name } = req.body;
+  try {
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+      return res.status(400).json({ error: "User already exists" });
     }
 
-}
+    const user = await User.create({ name, email, password });
+
+    const { accessToken, refreshToken } = generateTokens(user._id);
+    await storeRefreshToken(user._id, refreshToken);
+
+    setCookies(res, accessToken, refreshToken);
+
+    return res.status(201).json({
+      user: {
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        role: user.role,
+      },
+      message: "User created successfully",
+    });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
 
 export const login = async (req, res) => {
-    res.send('signup call');
-}
+  res.send("signup call");
+};
 
 export const logout = async (req, res) => {
-    res.send('signup call');
-}
+  res.send("signup call");
+};
--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,10 +1,10 @@
-import express from 'express';
-import {login, logout, signUp} from "../controllers/auth.controller.js";
+import express from "express";
+import { login, logout, signUp } from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
-router.post('/signup', signUp);
-router.post('/login', login);
-router.post('/logout', logout);
+router.post("/signup", signUp);
+router.post("/login", login);
+router.post("/logout", logout);
 
 export default router;
